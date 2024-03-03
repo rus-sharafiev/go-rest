633e0c4d561bb6f1b671ec53c997a289ca1bb7d3
Rus <rus.sharafiev@gmail.com>
2024-03-04 00:46:52 +0300

    Add log out path

--- common/auth/controller.go
+++ common/auth/controller.go
@@ -8,6 +8,7 @@ func authHandler() *http.ServeMux {
 	authRouter.Handle("/api/auth/signup", SignUp)
 	authRouter.Handle("/api/auth/signup/verify", VerifySignup)
 	authRouter.Handle("/api/auth/login", LogIn)
+	authRouter.Handle("/api/auth/logout", LogOut)
 	authRouter.Handle("/api/auth/user", GetAuthUser)
 	authRouter.Handle("/api/auth/refresh", Refresh)
 	authRouter.Handle("/api/auth/password/reset", ResetPassword)
