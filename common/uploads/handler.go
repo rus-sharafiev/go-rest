0748bea21f9807b2d0479d34150559cf8dba4642
Rus <rus.sharafiev@gmail.com>
2024-03-18 22:48:23 +0300

    Add upload path authentication

--- common/uploads/handler.go
+++ common/uploads/handler.go
@@ -3,10 +3,11 @@ package uploads
 import (
 	"net/http"
 	"path/filepath"
+	"strconv"
 
 	"github.com/rus-sharafiev/go-rest/common"
-	"github.com/rus-sharafiev/go-rest/common/auth"
 	"github.com/rus-sharafiev/go-rest/common/exception"
+	"github.com/rus-sharafiev/go-rest/common/jwt"
 )
 
 type handler struct{}
@@ -16,17 +17,25 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		exception.MethodNotAllowed(w)
 		return
 	}
-	userId, role := auth.Headers(r)
-	if len(userId) == 0 {
+
+	token := r.URL.Query().Get("token")
+	if len(token) == 0 {
 		exception.Unauthorized(w)
 		return
 	}
 
-	uploadDir := filepath.Join(*common.Config.StaticDir, *common.Config.UploadDir, userId)
-	if role == "ADMIN" {
-		uploadDir = filepath.Join(*common.Config.StaticDir, *common.Config.UploadDir)
+	claims, err := jwt.Validate(token)
+	if err != nil {
+		exception.BadRequestMessage(w, err.Error())
+		return
 	}
 
+	uploadDir := filepath.Join(*common.Config.UploadDir, strconv.Itoa(claims.UserId))
+	// if claims.UserAccess == "ADMIN" {
+	// 	uploadDir = filepath.Join(*common.Config.UploadDir)
+	// }
+
+	r.URL.RawQuery = ""
 	w.Header().Add("Cache-Control", "private, max-age=31536000, immutable")
 	http.StripPrefix(*common.Config.UploadPath, http.FileServer(http.Dir(uploadDir))).ServeHTTP(w, r)
 }
